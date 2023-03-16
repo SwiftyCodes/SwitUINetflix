870ed2ffeaf727c7daa09d6c9b3e96d2d262e6c2
Anurag <Swiftycodes@gmail.com>
2023-03-16 13:56:04 +0530

    Initial Commit

---
+++ NetflixSwiftUI/NetflixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSwiftUIApp.swift
+//  NetflixSwiftUI
+//
+//  Created by Anurag Kashyap on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
